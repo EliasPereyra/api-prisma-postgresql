bde53e79ef679f17ddb7a6e1306d2f6108883470
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-08 22:10:07 -0300

    Delete the previous code and replace it by importing express

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const PORT = 3005
 
 const prisma = new PrismaClient();
+const app = express()
 
-async function main() {
-  const newUser = await prisma.user.create({
-    data: {
-      name: 'elias',
-      email: 'elias@hotmail.com',
-      posts: {
-        create: {
-          title: "Hello world!"
-        }
-      }
-    }
-  })
+app.use(express.json())
 
-  console.log("User created", newUser)
-
-  const allUsers = await prisma.user.findMany({
-    include: { posts: true }
-  })
-
-  console.dir(allUsers, { depth: null, colors: true })
-}
-
-main()
-  .catch(e => console.error("There was an error: ", e))
-  .finally(async () => await prisma.$disconnect())
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`)
+})
