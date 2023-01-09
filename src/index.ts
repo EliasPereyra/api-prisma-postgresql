8327473400d08a611aa827268881ed4a3c86d5fa
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-08 22:01:23 -0300

    Create a user and a post

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,25 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'elias',
+      email: 'elias@hotmail.com',
+      posts: {
+        create: {
+          title: "Hello world!"
+        }
+      }
+    }
+  })
 
+  console.log("User created", newUser)
+
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true }
+  })
+
+  console.dir(allUsers, { depth: null, colors: true })
 }
 
 main()
